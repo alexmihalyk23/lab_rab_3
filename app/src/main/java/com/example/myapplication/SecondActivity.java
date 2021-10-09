3aea50c811158fcee196b44d8b833e0f83a853ba
alexmihalyk <alexmihalyk23@gmail.com>
2021-10-09 16:27:47 +0700

    update

--- app/src/main/java/com/example/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -1,7 +1,10 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,19 +17,18 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         final TextView textView = (TextView)findViewById(R.id.textView2);
-//        TextView textView = new TextView(this);
+
+
         textView.setTextSize(20);
         textView.setPadding(16, 16, 16, 16);
         textView.setText("Лабораторная работа № 3. Работу выполнил Михайлюк А.А.");
 
 
-//        setContentView(textView);
 
-//        setContentView(R.layout.activity_second);
-
-
-
-
+    }
+    public void BackButtonClick(View view){
+        Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
 
